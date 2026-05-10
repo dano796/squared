ffc6ed6817a3924588abbe435248c79277496db0
Emanuel0428 <cod2877@gmail.com>
2026-05-10 10:33:52 -0500

    chore: update Capacitor dependencies to version 8.3.3 and add @capacitor/assets

---
+++ android/app/src/main/java/com/daegame/squared/MainActivity.java
@@ -0,0 +1,5 @@
+package com.daegame.squared;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.daegame.squared',
+  appName: 'SquaredGame',
+  webDir: 'dist'
+};
+
+export default config;
